79cf03176ebf0dc15ecbbe99c356d88c01eab01f
Ahmad Ansori Palembani <palembani@gmail.com>
2026-07-29 22:17:58 +0700

    feat: Add A05s overlay
    This is based on furbanoramos21 and Tjari's fork of UN1CA, so credits to
    them and potentially to Salvo and Yagzie also.
    
    Co-authored-by: Salvo Giangreco <giangrecosalvo9@gmail.com>
    Co-authored-by: Yagzie <97476482+Yagzie@users.noreply.github.com>
    Co-authored-by: furbanoramos21 <107002046+furbanoramos21-testing@users.noreply.github.com>
    Co-authored-by: Tjari <zadigsm@outlook.com>

--- overlay.mk
+++ overlay.mk
@@ -238,6 +238,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-samsung-a02q \
 	treble-overlay-samsung-a02q-systemui \
 	treble-overlay-samsung-a03 \
+	treble-overlay-samsung-a05s \
 	treble-overlay-samsung-a10s \
 	treble-overlay-samsung-a20 \
 	treble-overlay-samsung-a20s \
