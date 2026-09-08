018ce2d44a41884166328bcbe16175e29bf9575d
Ahmad Ansori Palembani <palembani@gmail.com>
2026-09-08 12:40:59 +0700

    fix: Adjust a05s overlay
    Co-authored-by: Tjari <zadigsm@outlook.com>
    Co-authored-by: furbanoramos21 <107002046+furbanoramos21-testing@users.noreply.github.com>

--- build/build.sh
+++ build/build.sh
@@ -35,6 +35,7 @@ cd "$script_dir"
 
 echo "$makes" | while read -r f;do
     name="$(sed -nE 's/LOCAL_PACKAGE_NAME.*:\=\s*(.*)/\1/p' "$f")"
+    grep -q a05s <<<"$name" || continue
     grep -q treble-overlay <<<"$name" || continue
     echo "Generating $name"
 
